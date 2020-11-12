76a3bb4b4c2529c49a100446f97ea05a89dc4193
Andrea Fornaia <fornaia@dmi.unict.it>
2020-11-12 01:32:13 +0100

    Minor Fix on SommatoreParamTest

--- src/test/java/org/labisd/sum/SommatoreParamTest.java
+++ src/test/java/org/labisd/sum/SommatoreParamTest.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-@RunWith(value = Parameterized.class)
+@RunWith(Parameterized.class)
 public class SommatoreParamTest {
 	private int a;  // variabili usate nel test
 	private int b;
